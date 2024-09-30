3580534e5d1aef44e9042e049c7fe0dfb3a118e4
Ngoquangbao <144077695+Ngoquangbao@users.noreply.github.com>
2024-09-30 14:32:23 +0700

    Add files via upload

---
+++ JS-Operator/main.js
@@ -0,0 +1,22 @@
+var a = parseInt(prompt("Nhap a"))
+var b = parseInt(prompt("Nhap b"))
+// var a = 10
+// var b = 20
+var tong = a + b
+var hieu = a - b
+var tich = a * b
+var thuong = a / b
+document.write("Tong: " + tong + "<br>")
+document.write("Hieu: " + hieu + "<br>")
+document.write("Tich: " + tich + "<br>")
+document.write("Thuong: " + thuong + "<br>")
+
+document.write("****************<br>")
+var c = a > b
+document.write(a + " > " + b + " + " + c + "<br>")
+var c = a < b
+document.write(a + " < " + b + " + " + c + "<br>")
+var c = a >= b
+document.write(a + " >= " + b + " + " + c + "<br>")
+var c = a <= b
+document.write(a + " <= " + b + " + " + c + "<br>")
